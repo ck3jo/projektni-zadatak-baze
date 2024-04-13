716aee04c8082886d84f7de47545cb511f65c1e6
ckejo <jocic.andrija360@gmail.com>
2024-04-13 02:04:57 +0200

    boilerplate table views for edit and delete

--- database-gui/src/main/java/project/databasegui/DeleteController.java
+++ database-gui/src/main/java/project/databasegui/DeleteController.java
@@ -1,10 +1,27 @@
 package project.databasegui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class DeleteController
 {
+    public TableView tableViewAuthors;
+    public TableView tableViewPlayers;
+    public TableView tableViewMatches;
+    public TableView tableViewTeams;
+    public TableView tableViewTransfers;
+    public TableView tableViewCoaches;
+    public TableView tableViewTournaments;
+    public TableView tableViewNews;
+
     @FXML
     public void returnToMain() throws IOException { MainWindow.setRoot("main-window"); }
+
+    @FXML
+    public void sendAdjustedAuthorTableData() throws SQLException
+    {
+
+    }
 }
--- database-gui/src/main/java/project/databasegui/EditController.java
+++ database-gui/src/main/java/project/databasegui/EditController.java
@@ -1,10 +1,21 @@
 package project.databasegui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
 import java.io.IOException;
 
 public class EditController
 {
+    public TableView tableViewAuthors;
+    public TableView tableViewPlayers;
+    public TableView tableViewMatches;
+    public TableView tableViewTeams;
+    public TableView tableViewTransfers;
+    public TableView tableViewCoaches;
+    public TableView tableViewTournaments;
+    public TableView tableViewNews;
+
     @FXML
     public void returnToMain() throws IOException { MainWindow.setRoot("main-window"); }
 }
