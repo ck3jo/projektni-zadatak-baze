37c3bef99753e73c04b5947f5886e22af2de01d4
ckejo <jocic.andrija360@gmail.com>
2024-05-02 04:04:55 +0200

    basic laravel skeleton

--- database-web/routes/web.php
+++ database-web/routes/web.php
@@ -5,3 +5,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/authors', function () {
+    return view('authors');
+});
+
+Route::get('/players', function () {
+    return view('players');
+});
+
+Route::get('/matches', function () {
+    return view('matches');
+});
+
+Route::get('/teams', function () {
+    return view('teams');
+});
+
+Route::get('/transfers', function () {
+    return view('transfers');
+});
+
+Route::get('/coaches', function () {
+    return view('coaches');
+});
+
+Route::get('tournaments', function () {
+    return view('tournaments');
+});
+
+Route::get('news', function () {
+    return view('news');
+});
