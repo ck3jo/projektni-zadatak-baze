a9f79dffb5fe284c2daf768afa0a2800ddadd8e6
ckejo <jocic.andrija360@gmail.com>
2024-05-06 01:55:50 +0200

    slightly working laravel

--- database-web/resources/views/welcome.blade.php
+++ database-web/resources/views/welcome.blade.php
@@ -4,10 +4,22 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        @vite('resources/css/app.cs')
-        <title>Table example</title>
+        <title>FakeHLTV.org</title>
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body>  
-        <h1>Table example</h1>
+    <body class="bg-slate-900">  
+        <div class="py-3 px-2 bg-fuchsia-950/80 flex-auto flex-row justify-evenly self-center">
+            <a class="text-white font-bold px-5 hover:bg-fuschia-600" wire:navigate href="/">Glavna stranica</a>
+            <a class="text-white font-bold px-5 hover:bg-fuschia-600" wire:navigate href="/authors">Autori</a>
+            <a class="text-white font-bold px-5 hover:bg-fuschia-600" wire:navigate href="/players">Igrači</a>
+            <a class="text-white font-bold px-5 hover:bg-fuschia-600" wire:navigate href="/matches">Mečevi</a>
+            <a class="text-white font-bold px-5 hover:bg-fuschia-600" wire:navigate href="/teams">Timovi</a>
+            <a class="text-white font-bold px-5 hover:bg-fuschia-600" wire:navigate href="/transfers">Transferi</a>
+            <a class="text-white font-bold px-5 hover:bg-fuschia-600" wire:navigate href="/coaches">Treneri</a>
+            <a class="text-white font-bold px-5 hover:bg-fuschia-600" wire:navigate href="/tournaments">Turniri</a>
+            <a class="text-white font-bold pl-5 hover:bg-fuschia-600" wire:navigate href="/news">Vesti</a>
+        </div>
+        <h1 class="text-white font-bold ml-5 mt-5 text-7xl font-sans text-center">FakeHLTV.org</h1>
+        <h3 class="text-white ml-5 mt-5 text-xl font-sans text-center"> Aplikacija za evidentiranje profesionalnih CS2 mečeva</h3>
     </body>
 </html>
--- database-web/routes/web.php
+++ database-web/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\ShowAuthors;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/authors', [ShowAuthors::class, 'render']);
+?>
